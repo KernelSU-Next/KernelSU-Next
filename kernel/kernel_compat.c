1e0a6775372af15decacdf303adde9aeb62bd6db
holzip <104773229+holzip@users.noreply.github.com>
2026-04-03 17:03:46 +0300

    Deleting this block for kernel 4.9, as it causes a BUG when override_creds

--- kernel/kernel_compat.c
+++ kernel/kernel_compat.c
@@ -41,6 +41,7 @@ static int install_session_keyring(struct key *keyring)
 
 struct file *ksu_filp_open_compat(const char *filename, int flags, umode_t mode)
 {
+#if 0 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) ||                           \
 	defined(CONFIG_IS_HW_HISI) || defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)
 	if (init_session_keyring != NULL && !current_cred()->session_keyring &&
@@ -48,6 +49,7 @@ struct file *ksu_filp_open_compat(const char *filename, int flags, umode_t mode)
 		pr_info("installing init session keyring for older kernel\n");
 		install_session_keyring(init_session_keyring);
 	}
+#endif
 #endif
 
 	return filp_open(filename, flags, mode);
